dfc8f311f75f6e44969ae27b879524c7b941fc90
oyoungsun <plusmate@naver.com>
2023-02-10 19:56:00 +0900

    initalizer

---
+++ health-web/src/test/java/com/haedal/HealthWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haedal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
